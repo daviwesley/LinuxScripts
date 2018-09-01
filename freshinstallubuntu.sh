58409fa65a0c9a575ed11ec0e71186442e9bcdf5
Davi Wesley <davi.wesley@icloud.com>
2018-09-01 20:03:52 +0000

    Create freshinstallubuntu.sh

---
+++ freshinstallubuntu.sh
@@ -0,0 +1,10 @@
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt --fix-broken install
+sudo apt-get install build-essential git curl -y 
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+wget https://az764295.vo.msecnd.net/stable/493869ee8e8a846b0855873886fc79d480d342de/code_1.26.1-1534444688_amd64.deb
+dpkg -i code_1.26.1-1534444688_amd64.deb
+sudo apt --fix-broken install
+#sudo apt install python3-pip -y
