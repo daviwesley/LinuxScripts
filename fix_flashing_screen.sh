ef85c299e77f61ba7603202ea1525a0df803f6c3
Davi Wesley <davi.wesley@icloud.com>
2019-01-13 19:48:40 -0300

    Create fix_flashing_screen.sh
    after installing nvidia drivers you may have experience some issues on your screen like flashing screen, use this script to fix. tested on 
    NVIDIA Corporation GK107M [GeForce GT 730M] with nvidia-390 and nvidia-driver-390 on Elementray OS Juno

---
+++ fix_flashing_screen.sh
@@ -0,0 +1,2 @@
+# original thread on stackoverflow https://askubuntu.com/questions/881132/prime-displays-cannot-be-controlled-by-nvidia-settings
+echo options nvidia-drm modeset=1 >> /etc/modprobe.d/nvidia-drm-nomodeset.conf
