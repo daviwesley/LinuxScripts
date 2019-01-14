6251d4d525bb6bd61003530ede7444242dff99f8
Davi Wesley <davi.wesley@icloud.com>
2019-01-14 18:20:15 -0300

    Update fix_flashing_screen.sh

--- fix_flashing_screen.sh
+++ fix_flashing_screen.sh
@@ -1,2 +1,4 @@
 # original thread on stackoverflow https://askubuntu.com/questions/881132/prime-displays-cannot-be-controlled-by-nvidia-settings
 echo options nvidia-drm modeset=1 >> /etc/modprobe.d/nvidia-drm-nomodeset.conf
+# dont forget to update your boot img
+sudo update-initramfs -u
