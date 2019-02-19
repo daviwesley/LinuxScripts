eb551cd5c1cb75ee777741acafc7f4f6a8f0ac11
Davi Wesley <davi.wesley@icloud.com>
2019-02-19 10:44:50 -0300

    install mongodb

---
+++ install_mongodb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.2.22.tgz
+sudo mkdir /usr/local/mongodb
+sudo tar -zxvf mongodb-linux-x86_64-3.2.22.tgz -C /usr/local/mongodb
+
+# create enviroment variables
+echo " " >> ~/.bashrc # break a line
+echo 'export MONGODB_HOME=/usr/local/mongodb/mongodb-linux-x86_64-3.2.22/bin/' >> ~/.bashrc
+echo 'export PATH=$MONGODB_HOME:$PATH' >> ~/.bashrc
